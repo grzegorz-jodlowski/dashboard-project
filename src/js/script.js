fb166dc7d4ad1ce6ace7a190e193e1098f1cb1fb
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-02-03 21:14:03 +0100

    Fix bug with quotes

--- src/js/script.js
+++ src/js/script.js
@@ -37,24 +37,26 @@ for (let link of links) {
 
 var ctx = document.getElementById('myChart').getContext('2d');
 
+
+// eslint-disable-next-line no-undef, no-unused-vars
 var chart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
     datasets: [{
-      label: "Signups",
+      label: 'Signups',
       backgroundColor: '#8DBEC8',
       borderColor: '#8DBEC8',
       data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
     },
     {
-      label: "FTD",
+      label: 'FTD',
       backgroundColor: '#F29E4E',
       borderColor: '#F29E4E',
       data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
     },
     {
-      label: "Earned",
+      label: 'Earned',
       backgroundColor: '#71B374',
       borderColor: '#71B374',
       data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
